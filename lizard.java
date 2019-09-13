17faf3023dd27b8b0a1eb3cfaf77a256c9b6c04e
Eva Gleitz <eva_gleitz@m013393.aw.org>
2019-09-13 13:32:56 -0700

    figured it out

--- lizard.java
+++ lizard.java
@@ -1,5 +1,16 @@
+import java.util.*;
+
 public class lizard {
   public static void main(String[] args) {
-    System.out.println("Lizards are cool");
+    int n = 0;
+    System.out.println("Lizards are cool\n1)Yes\n2)No");
+    Scanner reader = new Scanner(System.in);
+    n = reader.nextInt();
+    if(n == 1) {
+      System.out.println("I approve");
+    }
+    if(n == 2) {
+      System.out.println("I disapprove");
+    }
   }
 }

e279877f8a82b0faa3fa333b0910afe1dd18fc95
Eva Gleitz <eva_gleitz@m013393.aw.org>
2019-09-13 14:16:43 -0700

    HW completed

---
+++ Response.java
@@ -0,0 +1,5 @@
+public class Response {
+  public static void main(String[] args) {
+    System.out.println("Small companies could invest in file recovery software. If the company is not able to afford it then it could work with other small companies to buy one. Similarly, they could invest in antivirus software and a firewall. The last suggestion would be performing regular backups, at least once a month, since these companies would be too small to effectively store in multiple locations.");
+  }
+}

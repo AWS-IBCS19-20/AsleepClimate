d6ad25165d10f74206ebd9955cb623fef89be585
Eva Gleitz <eva_gleitz@m013393.local>
2019-10-17 21:01:23 -0700

    test of command terms

--- lizard.java
+++ lizard.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class lizard {
   public static void main(String[] args) {
     int n = 0;
-    System.out.println("Lizards are cool\n1)Yes\n2)No");
+    System.out.println("Lizards are cool\n\n1)Yes\n\n2)No\n");
     Scanner reader = new Scanner(System.in);
     n = reader.nextInt();
     if(n == 1) {
@@ -11,6 +11,17 @@ public class lizard {
     }
     if(n == 2) {
       System.out.println("I disapprove");
+    }
+    System.out.println("Cats are better than dogs\n3)Yes\n4)No\n5)They taste the same");
+
+    if(n == 3) {
+      System.out.println("Opininated");
+    }
+    if (n == 4) {
+      System.out.println("Opininated");
+    }
+    if(n == 5) {
+      System.out.println("What is wrong with you");
     }
   }
 }
